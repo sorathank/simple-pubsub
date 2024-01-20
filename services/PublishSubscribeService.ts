e2fae85020fd1c1364733c36c63b28a1d615e338
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2024-01-20 13:29:55 +0700

    Apply event to queue PublishSubscribeService

--- services/PublishSubscribeService.ts
+++ services/PublishSubscribeService.ts
@@ -4,6 +4,8 @@ import IPublishSubscribeService from "./IPublishSubscribeService";
 
 class PublishSubscribeService implements IPublishSubscribeService {
   private subscribers: Map<string, ISubscriber[]> = new Map();
+  private eventQueue: IEvent[] = [];
+  private processingQueue: boolean = false;
 
   subscribe(type: string, handler: ISubscriber): void {
     const handlers = this.subscribers.get(type) || [];
@@ -20,10 +22,24 @@ class PublishSubscribeService implements IPublishSubscribeService {
   }
 
   publish(event: IEvent): void {
-    const handlers = this.subscribers.get(event.type()) || [];
-    handlers.forEach((handler) => handler.handle(event));
-    // Handle event queuing and processing logic
+    this.eventQueue.push(event);
+    if (!this.processingQueue) {
+      this.processEvents();
+    }
   }
+
+  private processEvents(): void {
+    this.processingQueue = true;
+    while (this.eventQueue.length > 0) {
+      const event = this.eventQueue.shift();
+      if (event) {
+        const handlers = this.subscribers.get(event.type()) || [];
+        handlers.forEach(handler => handler.handle(event));
+      }
+    }
+    this.processingQueue = false;
+  }
+  
 }
 
 export default PublishSubscribeService
