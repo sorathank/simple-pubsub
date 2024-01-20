5381ff82d18211ea2a489e1405ee32564ad04f7d
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2024-01-20 12:43:19 +0700

    Proper Handle RefillEvent

--- subscribers/MachineRefillSubscriber.ts
+++ subscribers/MachineRefillSubscriber.ts
@@ -2,9 +2,15 @@ import MachineRefillEvent from "../events/MachineRefillEvent";
 import ISubscriber from "./ISubscriber";
 
 class MachineRefillSubscriber implements ISubscriber {
-  handle(event: MachineRefillEvent): void {
-    throw new Error("Method not implemented.");
-  }
+    handle(event: MachineSaleEvent): void {
+        const machine = this.machines.find(m => m.id === event.machineId());
+        if (machine) {
+          machine.stockLevel -= event.getSoldQuantity();
+          if (machine.stockLevel < 3) {
+            this.pubSubService.publish(new LowStockWarningEvent(machine.id));
+          }
+        }
+      }
 }
 
 export default MachineRefillSubscriber;
