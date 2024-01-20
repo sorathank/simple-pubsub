a8134ce7a1641239440fba80b032c0ce9feda19b
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2024-01-20 12:09:08 +0700

    Roughly init PublishSubscribeService

---
+++ services/PublishSubscribeService.ts
@@ -0,0 +1,25 @@
+import IEvent from "../events/IEvent";
+import ISubscriber from "../subscribers/ISubscriber";
+import IPublishSubscribeService from "./IPublishSubscribeService";
+
+class PublishSubscribeService implements IPublishSubscribeService {
+    private subscribers: Map<string, ISubscriber[]> = new Map();
+  
+    subscribe(type: string, handler: ISubscriber): void {
+      const handlers = this.subscribers.get(type) || [];
+      handlers.push(handler);
+      this.subscribers.set(type, handlers);
+    }
+  
+    unsubscribe(type: string, handler: ISubscriber): void {
+      const handlers = this.subscribers.get(type) || [];
+      this.subscribers.set(type, handlers.filter(h => h !== handler));
+    }
+  
+    publish(event: IEvent): void {
+      const handlers = this.subscribers.get(event.type()) || [];
+      handlers.forEach(handler => handler.handle(event));
+      // Handle event queuing and processing logic
+    }
+  }
+  
