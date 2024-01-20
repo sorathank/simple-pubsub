9023e3a728ee81c167c9eae6256fa08353a270da
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2024-01-20 13:29:32 +0700

    Update test script

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import Machine from "./models/Machine";
 import IPublishSubscribeService from "./services/IPublishSubscribeService";
 import PublishSubscribeService from "./services/PublishSubscribeService";
+import MachineRefillSubscriber from "./subscribers/MachineRefillSubscriber";
 import MachineSaleSubscriber from "./subscribers/MachineSaleSubscriber";
+import StockWarningSubscriber from "./subscribers/StockWarningSubscriber";
 import { eventGenerator } from "./utils/eventGenerator";
 
-// program
+
 (async () => {
   // create 3 machines with a quantity of 10 stock
   const machines: Machine[] = [
@@ -12,14 +14,29 @@ import { eventGenerator } from "./utils/eventGenerator";
     new Machine("002"),
     new Machine("003"),
   ];
-  const pubSubService: IPublishSubscribeService = new PublishSubscribeService()
+  const pubSubService = new PublishSubscribeService();
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
+  /**
+   *  @Question back to author
+   *  What about the StockWarningSubscriber? I'm not sure why do we need to do this for this subscriber krub.😅
+   * */
   const saleSubscriber = new MachineSaleSubscriber(pubSubService, machines);
+  pubSubService.subscribe("MachineSaleEvent", saleSubscriber);
+
+  const machineRefillSubscriber = new MachineRefillSubscriber(
+    pubSubService,
+    machines
+  );
+  pubSubService.subscribe("MachineRefillEvent", machineRefillSubscriber);
+  
+  const stockWarningSubscriber = new StockWarningSubscriber();
+  pubSubService.subscribe("LowStockWarning", stockWarningSubscriber);
+  pubSubService.subscribe("StockLevelOk", stockWarningSubscriber);
 
   // create 5 random events
   const events = [1, 2, 3, 4, 5].map((i) => eventGenerator());
 
   // publish the events
-  events.map(pubSubService.publish);
+  events.forEach(event => pubSubService.publish(event));
 })();
